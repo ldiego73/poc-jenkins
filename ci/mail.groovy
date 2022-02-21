421430e6d73f5818c6aa4ba855933a66fa59a4f0
Luis Diego <lfdiego7@gmail.com>
2022-02-21 14:02:36 -0500

    feat: demo

--- ci/mail.groovy
+++ ci/mail.groovy
@@ -1,4 +1,8 @@
 def send(String to, String status) {
+    def body = sh(script: "cat ci/build.html", returnStdout: true).trim()
+    
+    echo "${body}"
+
     emailext body: "BUILD_NUMBER: $BUILD_NUMBER - BUILD_URL: $BUILD_URL - JOB_NAME: $JOB_NAME - GIT_COMMIT: $GIT_COMMIT - GIT_URL: $GIT_URL - GIT_BRANCH: $GIT_BRANCH",
         subject: "$PROJECT_NAME - Build #$BUILD_NUMBER - $status!",
         from: 'arquitectura.digital@auna.pe'
