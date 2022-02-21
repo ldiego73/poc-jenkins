c394acc42efd16df5b834200de82da9807659249
Luis Diego <lfdiego7@gmail.com>
2022-02-21 11:44:48 -0500

    feat: add email

--- ci/mail.groovy
+++ ci/mail.groovy
@@ -1,5 +1,5 @@
 def send(String to, String status) {
-    emailext body: 'Test Message',
+    emailext body: "BUILD_ID: $BUILD_ID - BUILD_NUMBER: $BUILD_NUMBER - BUILD_TAG: $BUILD_TAG - BUILD_URL: $BUILD_URL - JENKINS_URL: $JENKINS_URL - JOB_NAME: $JOB_NAME - GIT_COMMIT: $GIT_COMMIT - GIT_URL: $GIT_URL - GIT_BRANCH: $GIT_BRANCH",
         subject: "$PROJECT_NAME - Build #$BUILD_NUMBER - $status!",
         to: to;
 }
