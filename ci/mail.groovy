16545bb4fc068c16a371dccb2459b9cdac3605bc
Luis Diego <lfdiego7@gmail.com>
2022-02-21 14:06:35 -0500

    feat: demo

--- ci/mail.groovy
+++ ci/mail.groovy
@@ -1,11 +1,15 @@
 def send(String to, String status) {
     def body = sh(script: "cat ci/build.html", returnStdout: true).trim()
-    
-    echo "${body}"
 
+    emailext body: body.replaceAll("{{JobName}}", JOB_NAME),
+        subject: "$PROJECT_NAME - Build #$BUILD_NUMBER - $status!",
+        to: to;
+
+    /*
     emailext body: "BUILD_NUMBER: $BUILD_NUMBER - BUILD_URL: $BUILD_URL - JOB_NAME: $JOB_NAME - GIT_COMMIT: $GIT_COMMIT - GIT_URL: $GIT_URL - GIT_BRANCH: $GIT_BRANCH",
         subject: "$PROJECT_NAME - Build #$BUILD_NUMBER - $status!",
         to: to;
+    */
 }
 
 return this
